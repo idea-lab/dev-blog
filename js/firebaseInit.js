c7b9d27b18fe1cba70755fcf9c40fd3bb18dfd97
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2018-09-13 23:30:14 -0400

    Firebase Initialization

---
+++ js/firebaseInit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD5yqZNohAKxophHLLsjoa3iExFyuv1U5E",
+  authDomain: "idea-lab-blog.firebaseapp.com",
+  databaseURL: "https://idea-lab-blog.firebaseio.com",
+  projectId: "idea-lab-blog",
+  storageBucket: "idea-lab-blog.appspot.com",
+  messagingSenderId: "399735617360"
+};
+firebase.initializeApp(config);
